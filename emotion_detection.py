4bb6a8fd6b36ba4daa6fc9b8079e1c554401283f
Jeremy Cervantes <cervantes.jeremy.b@outlook.com>
2024-01-01 19:00:18 -0500

    changes made from theia lab environment

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json: { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(URL, json = input_json, headers=header)
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(URL, json = input_json, headers=Headers)
     formated_response = json.loads(response.text)
 
     if response.status_code == 200:
